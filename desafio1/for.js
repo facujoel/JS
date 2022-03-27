d975cb79617c1f69089107505bf37e666b40d1fd
Facu Moyano <facujoel1997@hotmail.com>
2022-03-26 23:18:43 -0300

    first commit

---
+++ desafio1/for.js
@@ -0,0 +1,22 @@
+
+
+let tengoHambre = parseInt(prompt("Tienes hambre? comer/no"));
+
+for (let i = 1; i <= 10; i++){
+
+    let resultado =  i + 0 ;
+
+    alert(" has comido " +  resultado  + " sanguchitos");
+
+    if(tengoHambre="comer"){
+
+    alert(" has comido " +  resultado  + " sanguchitos");}
+
+    while (tengoHambre !="no")
+
+        
+    alert(" ya no tienes hambre ");
+            
+    }; 
+
+
---
+++ index.js
@@ -0,0 +1,69 @@
+/* ------------------------------------------------------------------------- */
+/*  var nombre = "Facundo";
+var apellido = "Moyano"
+
+console.log(nombre);
+console.log(apellido);  */
+
+/*  let numero = prompt("ingrese un numero:");
+console.log(`la variable numero es de tipo ${typeof(numero)}`);
+console.log(`La salida convertida es ${typeof(parseInt(numero))}`); 
+ */
+    /* let numero1 = parseInt(prompt("ingrese un numero:"));
+    let numero2 = parseInt(prompt("ingrese un numero:"));
+    alert(`la suma de los numeros ingresados es: ${numero1 + numero2}`);
+ 
+/* ----------------------------------------------------------------------- */
+
+/*     function saludo(numero) {
+        for (let i = 0; i < numero; i++) {
+            
+            console.log("Hola");
+        }
+        
+    }
+    
+    saludo(10);
+     */
+
+
+    
+   /*  estructura de las funciones
+ */
+
+  /*  
+    function nombreFuncion(){
+        codigo a ejecutar
+    } */
+
+
+
+/*      function tengoHambre(){
+        let nombreIngresado = prompt("¿Tienes hambre? Y/N ");
+        alert("El nombre ingresado es " + nombreIngresado)
+    }
+    tengoHambre(); 
+ */
+
+  
+/* ----------------------------------------------------------------------- */
+
+/* let entrada = prompt("Tienes hambre? Yes/No");
+//Repetimos con While hasta que el usuario ingresa "ESC"
+while(entrada != "No" ){
+    alert("Aun tengo hambre ");
+    //Volvemos a solicitar un dato. En la próxima iteración se evalúa si no es ESC.
+    entrada = prompt("Sigues con hambre? Yes/No");
+}
+
+
+
+
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
